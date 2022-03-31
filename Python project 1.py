6b19f73edce13a2cc32fc70b1304c3f8765cf4cc
AdanAyub <102682526+AdanAyub@users.noreply.github.com>
2022-03-31 10:05:15 -0700

    Add files via upload

---
+++ Python project 1.py
@@ -0,0 +1,11 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+story = int(input("choose 1 or 2: "))
+if story == 1:
+    print(f"I love to {verb1}. I am so {adj}, and it is a lot of fun. It makes me act like {verb2} and I am a {famous_person}")
+else:
+    print(f"drawing is so when especially when I {adj}. I enjoy {verb1} in peace and I like to {verb2} when I am bored")
+
